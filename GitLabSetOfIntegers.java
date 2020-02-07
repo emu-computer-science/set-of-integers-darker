97085283d353122d098339db514865cb42942214
Parker <parker.allen702@gmail.com>
2020-02-07 15:00:58 -0800

    Add main and SetOfIntegers classes

---
+++ GitLabSetOfIntegers.java
@@ -0,0 +1,16 @@
+
+package gitlabsetofintegers;
+
+public class GitLabSetOfIntegers {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    private void TestSetOfIntegers()
+    {
+        String s = "9 9 9 9";
+        SetOfIntegers set = new SetOfIntegers(s);
+        System.out.println(set.Display());
+    }
+}
